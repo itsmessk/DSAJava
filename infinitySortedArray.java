05ddff139d825e534e657c8deff6c446866f3842
Sundar Karthik S <sundar1007karthik@gmail.com>
2025-01-07 02:12:35 +0530

    Implement search in an infinite sorted array using binary search

--- infinitySortedArray.java
+++ infinitySortedArray.java
@@ -1,3 +1,37 @@
 public class infinitySortedArray {
-    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 8 ,12, 45, 78, 98};
+        System.out.println(ans(arr, 1));
+    }
+
+    static int ans(int[] arr,int target){
+        int start = 0;
+        int end = 1;
+
+        while(target > arr[end]){
+            int newStart = end + 1;
+            end = start + (end - start + 1) * 2;
+            start = newStart;
+        }
+
+        return binarySearch(arr, target, start, end);
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end){
+
+        while(start<= end){
+            int mid = start + (end - start) / 2;
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
